ef5aaf1528208634af521ef43eb1b90cfcd971e5
leonardoosse <leonardo.osse@gmail.com>
2021-06-03 10:10:49 -0300

    API Cotacao

---
+++ api-cotacao/APIConversao/Controllers/Conversao.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APIConversao.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace APIConversao.Controllers
+{
+    [ApiController]
+    [Route("conversao")]
+    public class Conversao : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public string Get()
+        {
+            return "Ok";
+        }
+
+        [HttpPost]
+        [Route("calcular")]
+        public JsonResult Calcular([FromBody] Valor valor)
+        {
+            return valor.Calcular();            
+        }
+    }
+}
---
+++ api-cotacao/APIConversao/Model/Valor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIConversao.Model
+{
+    public class Valor
+    {
+        public Valor() { }
+        
+        public decimal taxaMoeda { get; set; }
+        public decimal taxaSegmento { get; set; }
+        public decimal valorDesejado { get; set; }
+
+        public JsonResult Calcular()
+        {
+            decimal valorCalculado = (this.valorDesejado * this.taxaMoeda) * (1 + this.taxaSegmento);
+
+            return new JsonResult(new
+            {
+                valorCalculado = valorCalculado
+            }); 
+        }
+    }
+}
